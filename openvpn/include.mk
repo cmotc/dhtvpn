39807d10d73bcf6b0ad2cb4409abf7aafd705021
idk <idk@fireaxe.local>
2017-07-15 10:06:03 -0400

    add and remove ratox user from grsec-tpe for the purposes of compiling an updated ratox

--- openvpn/include.mk
+++ openvpn/include.mk
@@ -1,3 +1,15 @@
-
 build-openvpn:
 	cd openvpn && docker build -t alpine-openvpn .
+
+enter-openvpn:
+	docker run -i --rm --name alpine-openvpn -t alpine-openvpn /bin/sh
+
+enter-running-openvpn:
+	docker exec -i -t alpine-openvpn /bin/sh
+
+run-openvpn:
+	docker run -id \
+		--name alpine-openvpn \
+		--network dhtvpn-network \
+		--ip 192.168.5.3 \
+		-t alpine-openvpn openvpn
